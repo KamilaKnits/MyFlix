010563d9bd3ab53a322a0528ab5f5b3bd1e70467
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-02-03 21:41:44 -0500

    Update index.js
    reenabled token based authentication on /movies endpoint

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 
 // get list of all movies//
 
-app.get('/movies', 
+app.get('/movies', passport.authenticate('jwt', { session: false }),
     async (req, res) => {
         await Movies.find()
             .then((movies) => {
